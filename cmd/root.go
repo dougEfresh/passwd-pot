0602f9b9e51112b84c2377667b63429d9c027772
Douglas Chimento <dchimento@gmail.com>
2017-03-08 20:01:13 -0500

    fmt

--- cmd/root.go
+++ cmd/root.go
@@ -75,8 +75,8 @@ func initConfig() {
 	}
 
 	viper.SetConfigName(".passwd-pot") // name of config file (without extension)
-	viper.AddConfigPath("$HOME")          // adding home directory as first search path
-	viper.AutomaticEnv()                  // read in environment variables that match
+	viper.AddConfigPath("$HOME")       // adding home directory as first search path
+	viper.AutomaticEnv()               // read in environment variables that match
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
