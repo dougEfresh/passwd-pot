5ba69fa303ab78b97f559523869bea89cfa34c49
Doug Chimento <dchimento@gmail.com>
2018-03-11 22:01:01 +0000

    error checking

--- lambda/handler.go
+++ lambda/handler.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -16,28 +17,28 @@ var eventResolver service.EventResolver
 var eventClient *service.EventClient
 var logger log.Logger
 var dsn = os.Getenv("PASSWDPOT_DSN")
+var setupError error
 
 func init() {
-	var err error
-	logger = log.Logger{}
 	logger.SetLevel(log.InfoLevel)
-	if os.Getenv("PASSWDPOT_DEBUG") == "1" {
-		logger.SetLevel(log.DebugLevel)
-	}
 	logger.AddLogger(klog.NewJSONLogger(os.Stdout))
 	logger.With("app", "passwdpot-create-event")
 	logger.With("ts", klog.DefaultTimestampUTC)
 	logger.With("caller", klog.Caller(4))
-	eventResolver, err = service.NewResolveClient(service.WithResolvDsn(dsn), service.SetGeoDb("geo.mmdb.gz"))
+	var err error
+	if os.Getenv("PASSWDPOT_DEBUG") == "1" {
+		logger.SetLevel(log.DebugLevel)
+	}
+	eventResolver, err = service.NewResolveClient(service.WithResolvDsn(dsn))
 	if err != nil {
 		logger.Errorf("Error setting up client %s", err)
-	}
-	if err != nil {
-		logger.Errorf("DB error %s", err)
+		setupError = errors.New(fmt.Sprintf("resolver has bad setup %s", err))
+		return
 	}
 	eventClient, err = service.NewEventClient(service.SetEventLogger(logger), service.WithDsn(dsn))
 	if err != nil {
 		logger.Errorf("Error setting up eventClient %s", err)
+		setupError = errors.New(fmt.Sprintf("eventClient  has bad setup %s", err))
 	}
 }
 
@@ -47,6 +48,12 @@ type ApiEvent struct {
 
 func Handle(apiEvent ApiEvent) (events.APIGatewayProxyResponse, error) {
 	e := apiEvent.Event
+	if e.RemoteAddr == "" {
+		return events.APIGatewayProxyResponse{Body: fmt.Sprintf("error  with event %s", e), StatusCode: 500}, errors.New("Invalid event")
+	}
+	if setupError != nil {
+		return events.APIGatewayProxyResponse{Body: "Bad setup", StatusCode: 500}, setupError
+	}
 	logger.Debugf("Event %s", e)
 	id, err := eventClient.RecordEvent(e)
 	if err != nil {
