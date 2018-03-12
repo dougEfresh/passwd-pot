7861edc62260c53714c1894a9103aa092a94a7ca
Douglas Chimento <dchimento@gmail.com>
2018-03-12 12:04:13 +0200

    using small time

--- lambda/deploy.sh
+++ lambda/deploy.sh
@@ -7,4 +7,4 @@ rm -f passwdpot.zip 2> /dev/null
 zip passwdpot.zip  handler
 $dockerRun --region $REGION  s3 cp /tmp/aws/passwdpot.zip s3://lambda-passwdpot-eu-central-1/passwdpot.zip && \
 $dockerRun --region $REGION lambda update-function-code --function-name passwdpot-create-event --s3-bucket lambda-passwdpot-eu-central-1  --s3-key passwdpot.zip && \
-$dockerRun --region $REGION lambda invoke /dev/stdout  --function-name passwdpot-create-event --invocation-type RequestResponse  --payload '{ "event":  { "time": 1487973301661, "user": "admin", "passwd": "12345678", "remoteAddr": "158.69.243.135", "remotePort": 63185, "remoteName": "203.116.142.113", "remoteVersion": "SSH-2.0-JSCH-0.1.51", "application": "OpenSSH", "protocol": "ssh" , "originAddr": "203.116.142.113" }}'
+$dockerRun --region $REGION lambda invoke /dev/stdout  --function-name passwdpot-create-event --invocation-type RequestResponse  --payload '{ "event":  { "time": 2, "user": "admin", "passwd": "12345678", "remoteAddr": "158.69.243.135", "remotePort": 63185, "remoteName": "203.116.142.113", "remoteVersion": "SSH-2.0-JSCH-0.1.51", "application": "OpenSSH", "protocol": "ssh" , "originAddr": "203.116.142.113" }}'
