a55f404425cbf94e0373dd8268d4cdd56b06ad8d
Doug Chimento <dchimento@gmail.com>
2018-03-16 15:26:06 +0200

    moving origin

--- lambda/update-lambda-code.sh
+++ lambda/update-lambda-code.sh
@@ -7,6 +7,6 @@ rm -f passwdpot.zip 2> /dev/null
 zip passwdpot.zip  handler
 $dockerRun --region $REGION  s3 cp /tmp/aws/passwdpot.zip s3://passwdpot-$REGION/passwdpot.zip && \
 $dockerRun --region $REGION lambda update-function-code --publish --function-name passwdpot-create-event --s3-bucket passwdpot-$REGION  --s3-key passwdpot.zip && \
-$dockerRun --region $REGION lambda invoke /dev/stdout  --function-name passwdpot-create-event --invocation-type RequestResponse  --payload '{ "originAddr": "203.116.142.113",  "event":  { "time": 2, "user": "admin", "passwd": "12345678", "remoteAddr": "158.69.243.135", "remotePort": 63185, "remoteName": "203.116.142.113", "remoteVersion": "SSH-2.0-JSCH-0.1.51", "application": "OpenSSH", "protocol": "ssh"}}'
+$dockerRun --region $REGION lambda invoke /dev/stdout  --function-name passwdpot-create-event --invocation-type RequestResponse  --payload '{ "event":  { "originAddr": "203.116.142.113", "time": 2, "user": "admin", "passwd": "12345678", "remoteAddr": "158.69.243.135", "remotePort": 63185, "remoteName": "203.116.142.113", "remoteVersion": "SSH-2.0-JSCH-0.1.51", "application": "OpenSSH", "protocol": "ssh"}}'
 
 #$dockerRun --region $REGION cloudformation update-stack --stack-name passwdpot-api-lambda  --template-body file:///tmp/aws/passwdpot-template.json  --capabilities CAPABILITY_IAM --parameters ParameterKey=PasswdPotDsn,ParameterValue="'$PASSWDPOT_DSN'" ParameterKey=S3Bucket,UsePreviousValue=true ParameterKey=S3Key,UsePreviousValue=true ParameterKey=Debug,UsePreviousValue=true
