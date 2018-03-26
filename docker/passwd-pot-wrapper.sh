2d0f9c78716a8edd2d49d7d428db6406e3272c92
Doug Chimento <dchimento@gmail.com>
2018-03-26 14:13:47 +0300

    add docker build for travis

--- docker/passwd-pot-wrapper.sh
+++ docker/passwd-pot-wrapper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-set -x
 nohup su -s /bin/bash nobody -c "/bin/passwd-pot potter $PASSWD_POT_OPTS" > /passwd-pot.log &
 nohup su -s /bin/bash nobody -c "/bin/passwd-pot socket $PASSWD_POT_SOCKET_OPTS" > /passwd-pot-socket.log &
+sleep 2
